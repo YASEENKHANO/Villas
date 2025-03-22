6ee4f3d0e925a35818c9fad73d9be53740d6be64
M. Yaseen Khan <myaseen.khan9498360@gmail.com>
2025-03-22 22:04:04 +0500

    Add project files.

---
+++ GulfVillas.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GulfVillas.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
